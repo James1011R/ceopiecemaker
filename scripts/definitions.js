cb0ef1ce55142b4259a239ea9267a7c5af9f1eae
James1011R <31488779+James1011R@users.noreply.github.com>
2017-09-25 01:19:12 -0400

    Warp Swap is no longer invisible

--- scripts/definitions.js
+++ scripts/definitions.js
@@ -863,7 +863,7 @@ MOVES = [{
     "name": "warpswap",
     "long": "b]mz:move/attack/swap",
     "text": "(Unstoppable) Move, Attack or swap places with ally.",
-    "color": [180, 210, 255],
+    "color": [160, 210, 255],
     "hide": true
 }, {
     "id": "35b",
